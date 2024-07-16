f75e902dde959c8ecd4ea499a0d847e85fc20ce4
sploicers <7642631+sploicers@users.noreply.github.com>
2024-07-17 00:11:26 +1200

    Significant improvements

--- payslips.js
+++ payslips.js
@@ -10,104 +10,123 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js
 // ==/UserScript==
 
-(function () {
-	'use strict';
-	const TABLE_HEADER_ROW_COUNT = 2;
-	const TABLE_ROW_CHILD_NODE_INDEX = 5;
-	const DOM_POLL_INTERVAL_MS = 500;
-	const NUM_SIMULTANEOUS_DOWNLOADS = 5;
-	const DOWNLOAD_BATCH_DELAY_MS = 1000;
-	const DEBUG = false;
-	//https://payroll.ascenderpay.com/vzbp-wss/faces/WJ0000
+(async function () {
+  'use strict';
+  const TABLE_HEADER_ROW_COUNT = 2;
+  const TABLE_ROW_CHILD_NODE_INDEX = 5;
+  const DOM_POLL_INTERVAL_MS = 500;
+  const MAX_SIMULTANEOUS_DOWNLOADS = 5;
+  const DOWNLOAD_BATCH_DELAY_MS = 1000;
+  const DRY_RUN = true;
 
-	const alreadyFollowedIframeLink = window.location.href.includes('ords/wss_vzbp');
-	if (alreadyFollowedIframeLink) {
-		const rowSelector = "table[summary='Pehistpay'] tr";
-		waitForElementsBySelector(rowSelector, rows =>
-			downloadAllPayslips(rows).catch(console.error));
+  const onPayslipHistoryPage = window.location.href.includes('ords/wss_vzbp');
+  if (onPayslipHistoryPage) {
+    await downloadAllPayslips().catch(console.error);
+  } else {
+    await navigateToPayslipHistoryPage().catch(console.error);
+  }
 
-	} else {
-		const iframeSelector = "iframe[title='WSS page content']";
-		waitForSingleElementBySelector(iframeSelector, navigateToIframe);
-	}
+  async function navigateToPayslipHistoryPage() {
+    const myDetailsMenu = await waitForElementById("pt1:pt_sdi8::btn");
+    myDetailsMenu.click();
 
-	async function downloadAllPayslips(tableRows) {
-		const domParser = new DOMParser();
-		addHtml2PdfScriptToPage();
+    const tableCells = await waitForElementsBySelector("div[id='pt1:pt_pfl1'] td");
+    const employeeDetails = tableCells[0].innerText.trim(); // EmployeeID LastName, FirstName MiddleName(s)
+    const employeeId = employeeDetails.split(' ')[0];
 
-		// Get every relevant table row - ditch the column headers and the "Year to Date" row.
-		const relevantRows = [...tableRows].slice(TABLE_HEADER_ROW_COUNT);
-		// Grab the cells belonging to the "Period End" column in the table, and get the corresponding links.
-		const links = relevantRows.flatMap(row => [...row.childNodes][TABLE_ROW_CHILD_NODE_INDEX].childNodes[0]);
-		// Download.
-		debugBreakpoint();
-		for (const chunk of chunkArray(links, NUM_SIMULTANEOUS_DOWNLOADS)) {
-			await Promise.all(chunk.map(link => downloadPayslip(link, domParser)));
-			await delayMs(DOWNLOAD_BATCH_DELAY_MS);
-		}
-	}
+    const baseUrl = 'https://payroll.ascenderpay.com';
+    const payslipHistoryPath = 'ords/wss_vzbp/WK8020VZ$.startup'
+    window.location.href = `${baseUrl}/${payslipHistoryPath}?Z_EMPLOYEE_NUMBER=${employeeId}&P_MODE=R`;
+  }
 
-	function navigateToIframe(iframe) {
-		window.location.href = iframe.src;
-	}
+  async function downloadAllPayslips() {
+    const tableRows = await waitForElementsBySelector("table[summary='Pehistpay'] tr");
+    // Ditch the column headers and the "Year to Date" row.
+    const relevantRows = tableRows.slice(TABLE_HEADER_ROW_COUNT);
+    // Grab the cells belonging to the "Period End" column in the table, and get the corresponding links.
+    const links = relevantRows.flatMap(row => [...row.childNodes][TABLE_ROW_CHILD_NODE_INDEX].childNodes[0]);
+    // Download.
+    const domParser = new DOMParser();
+    addHtml2PdfScriptToPage();
+    for (const chunk of chunkArray(links, MAX_SIMULTANEOUS_DOWNLOADS)) {
+      await Promise.all(chunk.map(link => downloadSinglePayslip(link, domParser)));
+      await delayMs(DOWNLOAD_BATCH_DELAY_MS);
+    }
+  }
 
-	async function downloadPayslip(link, domParser) {
-		try {
-			// 'innerText' here is the payslip date - use this as the filename.
-			const {href: url, innerText: filename} = link;
-			const pageContent = await fetch(url).then(response => response.text());
-			const document = domParser.parseFromString(pageContent, 'text/html');
-			await html2pdf(document.body).save(filename);
-		} catch (e) {
-			console.error(e);
-			debugBreakpoint();
-		}
-	}
+  async function downloadSinglePayslip(link, domParser) {
+    try {
+      const {href: url, innerText: periodEndDate} = link;
+      const filename = `${periodEndDate}.pdf`;
+      console.log(`About to download ${filename}. Dry run: ${DRY_RUN}`);
 
-	function waitForElementsBySelector(querySelector, onReady, onNotReady) {
-		const poll = window.setInterval(inner, DOM_POLL_INTERVAL_MS);
-		function inner() {
-			const elementReady = document.querySelector(querySelector) !== null;
-			if (elementReady) {
-				window.clearInterval(poll);
-				// DOM elements are returned in a NodeList, but we want just a normal JS array.
-				const elements = [...document.querySelectorAll(querySelector)];
-				debugBreakpoint();
-				onReady(elements);
-			} else {
-				debugLog(`No matches in DOM for selector ${querySelector}. Retrying in ${DOM_POLL_INTERVAL_MS}ms.`);
-				onNotReady?.();
-			}
-		}
-	}
+      if (!DRY_RUN) {
+        const pageContent = await fetch(url).then(response => response.text());
+        const document = domParser.parseFromString(pageContent, 'text/html').body;
+        await html2pdf(document)
+          .set({
+            letterRendering: true,
+            dpi: 300,
+            scale: 4
+          })
+          .save(`${periodEndDate}.pdf`);
+      }
 
-	function waitForSingleElementBySelector(querySelector, onReady, onNotReady) {
-		const newOnReadyCallback = (results) => onReady(results?.[0]);
-		waitForElementsBySelector(querySelector, newOnReadyCallback, onNotReady);
-	}
+    } catch (e) {
+      console.error(e);
+      debugger;
+    }
+  }
 
-	// We make use of the "html2pdf" NPM package - add its bundle to the page in a <script> tag, so
-	// that the 'html2pdf' function is accessible later.
-	function addHtml2PdfScriptToPage() {
-		const script = document.createElement('script');
-		script.type = 'application/javascript';
-		script.src = 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js';
-		document.head.appendChild(script);
-	}
+  function waitForElementsBySelector(querySelector) {
+    return new Promise(resolve => {
+      const poll = window.setInterval(inner, DOM_POLL_INTERVAL_MS);
+      function inner() {
+        // DOM elements are returned in a NodeList, but we want just a normal JS array.
+        const elements = [...document.querySelectorAll(querySelector)];
+        const elementsReady = elements.length > 0;
+        if (elementsReady) {
+          window.clearInterval(poll);
+          resolve(elements);
+        } else {
+          console.log(`No matches in DOM for selector ${querySelector}. Retrying in ${DOM_POLL_INTERVAL_MS}ms.`);
+        }
+      }
+    });
+  }
 
-	function* chunkArray(items, n) {
-		for (let i = 0; i < items.length; i += n) {
-			yield items.slice(i, i + n);
-		}
-	}
+  function waitForElementById(id) {
+    return new Promise(resolve => {
+      const poll = window.setInterval(inner, DOM_POLL_INTERVAL_MS);
+      function inner() {
+        const element = document.getElementById(id);
+        const elementReady = element !== null;
+        if (elementReady) {
+          window.clearInterval(poll);
+          resolve(element);
+        } else {
+          console.log(`No matches in DOM for id ${id}. Retrying in ${DOM_POLL_INTERVAL_MS}ms.`);
+        }
+      }
+    });
+  }
 
-	function delayMs(ms) {
-		return new Promise(resolve => setTimeout(resolve, ms));
-	}
-	function debugBreakpoint() {
-		if (DEBUG) debugger;
-	}
-	function debugLog(msg) {
-		if (DEBUG) console.log(msg);
-	}
+  // We make use of the "html2pdf" NPM package - add its bundle to the page in a <script> tag, so
+  // that the 'html2pdf' function is accessible later.
+  function addHtml2PdfScriptToPage() {
+    const script = document.createElement('script');
+    script.type = 'application/javascript';
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js';
+    document.head.appendChild(script);
+  }
+
+  function* chunkArray(items, n) {
+    for (let i = 0; i < items.length; i += n) {
+      yield items.slice(i, i + n);
+    }
+  }
+
+  function delayMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 })();
